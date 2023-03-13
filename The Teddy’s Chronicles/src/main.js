45724ec782d0cc2657e8cd336d1d19037fb53f92
kevyx-gif <apolortms@gmail.com>
2023-03-12 18:37:08 -0600

    Base Del Juego  v1.0

--- The Teddy’s Chronicles/src/main.js
+++ The Teddy’s Chronicles/src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-
 import './assets/main.css'
+import * as PIXI from 'pixi.js'
 
 const app = createApp(App)
-
-app.use(router)
-
+app.config.globalProperties.PIXI = PIXI
 app.mount('#app')
